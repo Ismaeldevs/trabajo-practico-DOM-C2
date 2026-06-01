50229a2d542f63274ef228a63401aaa3bba0ae8d
Juan David Lobo <juamdaviid@gmail.com>
2026-06-01 01:28:02 -0300

    Trabajo Practico Realizado.

---
+++ 63661-Lobo-Juan-David/Ejercicio4/script4.js
@@ -0,0 +1,12 @@
+const inputNombre = document.getElementById("inputNombre");
+const preview = document.getElementById("preview");
+
+inputNombre.addEventListener("input", function () {
+    const nombre = inputNombre.value;
+
+    if(nombre === ""){
+        preview.textContent = "Escribe tu nombre arriba";
+    }else{
+        preview.textContent = "Hola, "+ nombre +" Maquina!";
+    }
+});
